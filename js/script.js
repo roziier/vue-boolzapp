8b1aa4fe9bb2adee7aba44914511e8051aa83fd0
roziier <rikirosa93@gmail.com>
2020-11-23 13:30:39 +0100

    last commit.. trying to use v-for

---
+++ js/script.js
@@ -0,0 +1,38 @@
+new Vue({
+  el: '#app',
+  data: {
+    items: [
+
+    // FIRST
+    {
+      name: 'Pierangelo',
+      img: 'img/avatar_1.jpg',
+      lastseen: '21/11/20 at 20.00pm',
+      msg: 'Ciao come stai?',
+      first: true
+    },
+
+    // SECOND
+    {
+      name: 'Giuseppe',
+      img: 'img/avatar_2.jpg',
+      lastseen: '21/11/20 at 19.00pm',
+      msg: 'Bene tu?',
+      second: true
+    },
+
+    // THIRD
+    {
+      name: 'Claudia',
+      img: 'img/avatar_3.jpg',
+      lastseen: '21/11/20 at 18.00pm',
+      msg: 'Abbastanza bene dai',
+      third: true
+    }
+  ],
+
+    methods: {
+
+    }
+  }
+})
