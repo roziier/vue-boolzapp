8c056ebcab95029d0d1a5d2528018f1a6f9500a2
roziier <rikirosa93@gmail.com>
2020-11-25 17:30:20 +0100

    chat

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ var app = new Vue({
     {
       name: 'Pierangelo',
       img: 'img/avatar_1.jpg',
-
+      lastview: '21/11/20 8.30am',
       chat: [
       {
         messaggio: 'Come stai oggi?',
         orario: '21/11/20 8:00am',
-        stato: 'inviato'
+        stato: true
       },
       {
         messaggio: 'sto bene grazie',
         orario: '21/11/20 8:10am',
-        stato: 'ricevuto'
+        stato: false
       }
       ]
     },
@@ -27,16 +27,17 @@ var app = new Vue({
     {
       name: 'Giuseppe',
       img: 'img/avatar_2.jpg',
+      lastview: '21/11/20 8.00am',
       chat: [
       {
         messaggio: 'Come stai oggi?',
         orario: '21/11/20 8:30am',
-        stato: 'inviato'
+        stato: false
       },
       {
         messaggio: 'cosi cosi',
         orario: '21/11/20 8:40am',
-        stato: 'ricevuto'
+        stato: true
       }
       ]
     }
